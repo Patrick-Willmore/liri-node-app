189386bfa2d93aa9ee644e0783f579aad43305d8
Patrick-Willmore <patman1127@gmail.com>
2018-01-27 11:26:45 -0700

    file structure

---
+++ keys.js
@@ -0,0 +1,26 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'welzF2wwSCySEFQnlghsVFonA',
+  consumer_secret: 'Xk5Uc9PjNd2mdsEKQK3RM8bFIGQWGQ4o8OjFEzg35OdzqhYgVa',
+  access_token_key: '957310239874867201-WChcgbmVD6iLceBIH5F6ldI7xkyHSfH',
+  access_token_secret: 'jmmRUICCKsfDa0RGhk5hXZNGAtuLV4m7lu28POC5XsTYd',
+}
+
+module.exports = twitterKeys;
+
+// Your Access Token
+// This access token can be used to make API requests on your own account's behalf. Do not share your access token secret with anyone.
+// Access Token	957310239874867201-WChcgbmVD6iLceBIH5F6ldI7xkyHSfH
+// Access Token Secret	jmmRUICCKsfDa0RGhk5hXZNGAtuLV4m7lu28POC5XsTYd
+// Access Level	Read and write
+// Owner	Petewilliams675
+// Owner ID	957310239874867201
+
+// Application Settings
+// Keep the "Consumer Secret" a secret. This key should never be human-readable in your application.
+// Consumer Key (API Key)	welzF2wwSCySEFQnlghsVFonA
+// Consumer Secret (API Secret)	Xk5Uc9PjNd2mdsEKQK3RM8bFIGQWGQ4o8OjFEzg35OdzqhYgVa
+// Access Level	Read and write (modify app permissions)
+// Owner	Petewilliams675
+// Owner ID	957310239874867201
